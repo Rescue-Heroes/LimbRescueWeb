a0b1ff8a63a196a2b6609a1b4c3323d2daf4da61
BrowyLi <70491686+BrowyLi@users.noreply.github.com>
2021-07-25 14:34:56 -0400

    Create chatbot.py
    chatbot

---
+++ LimbRescueWeb/chatbot.py
@@ -0,0 +1,30 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Creating ChatBot Instance
+chatbot = ChatBot('DoctorBot')
+
+ # Training with Personal Ques & Ans
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome.",
+    "What is lymphedema?",
+    "Lymphedema, or lymphatic obstruction, is a long-term condition where excess fluid collects in tissues causing swelling (edema).",
+    "How should I use the website?",
+    "You can use smartwatches to test you PPG waveform and input the result into the website. We will generate a diagnosis for you."
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
+
+# Training with English Corpus Data
+trainer_corpus = ChatterBotCorpusTrainer(chatbot)
+trainer_corpus.train(
+    'chatterbot.corpus.english'
+)
